5258134e5941bece205fa6a679e597654b21f667
Aryan Sharma <ryn2624@icloud.com>
2020-04-30 12:36:29 +0530

    basic test

--- Text-Editor-Test/ViewController.swift
+++ Text-Editor-Test/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let rtView = RichTextEditor(frame: self.view.frame)
+        self.view.addSubview(rtView)
     }
 
 
